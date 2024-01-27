66891b9ad395a4d72f8a10d301f5219ba815d8a1
Bobshott <145829787+benjaminbob21@users.noreply.github.com>
2024-01-27 12:25:44 -0600

    Add files via upload

---
+++ Hurdle 1 (1).py
@@ -0,0 +1,19 @@
+def roun():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+for i in range(6):
+    roun()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
